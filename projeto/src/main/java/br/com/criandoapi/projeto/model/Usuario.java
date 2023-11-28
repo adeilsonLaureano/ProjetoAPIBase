853408ffcafc70e225432d68e5da5ec0ce0849fa
adeilsonLaureano <adeilson.dede2018@gmail.com>
2023-11-27 22:57:32 -0300

    lombok

--- projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
 
 @Entity
 @Table(name = "usuario")
@@ -22,46 +25,4 @@ public class Usuario {
     private String senha;
     @Column(name = "telefone", length = 45, nullable = true)
     private String telefone;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    
 }
