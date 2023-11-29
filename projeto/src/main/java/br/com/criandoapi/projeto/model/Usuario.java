78456da841a5e58b047ccd12df03508ee2eab1a3
adeilsonLaureano <adeilson.dede2018@gmail.com>
2023-11-28 21:15:09 -0300

    Melhorias/Validation

--- projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -6,6 +6,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -17,12 +20,22 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+
+    @NotBlank(message = "O nome é obrigatório!")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres!")
     @Column(name = "nome", length = 45, nullable = true)
     private String nome;
+
+    @Email(message = "Insira um email válido!")
+    @NotBlank(message = "O email é obrigatório!")
     @Column(name = "email", length = 45, nullable = true)
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória!")
     @Column(name = "senha", columnDefinition = "TEXT", nullable = true)
     private String senha;
+
+    @NotBlank(message = "O telefone é obrigatório!")
     @Column(name = "telefone", length = 45, nullable = true)
     private String telefone;
 }
